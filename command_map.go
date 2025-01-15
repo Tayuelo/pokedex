fc734de6957f15ecfd53fdedf8ca17421c3d7fda
Aljadis Tavera <tayuelo26@gmail.com>
2025-01-15 06:49:12 -0500

    Find pokemon encounters per area

--- command_map.go
+++ command_map.go
@@ -15,7 +15,7 @@ type PokeResponse struct {
 	} `json:"results"`
 }
 
-func commandMap(cfg *config) error {
+func commandMap(cfg *config, args ...string) error {
 	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 	if err != nil {
 		return err
@@ -30,7 +30,7 @@ func commandMap(cfg *config) error {
 	return nil
 }
 
-func commandMapb(cfg *config) error {
+func commandMapb(cfg *config, args ...string) error {
 	if cfg.prevLocationsURL == nil {
 		return errors.New("you're on the first page")
 	}
